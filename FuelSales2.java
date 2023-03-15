9048a6245b9abe05e76902c3e3e108a43a6fc611
@Bahati308 <bahatibrianp@gmail.com>
2023-03-15 17:23:02 +0300

    modified FuelSales2.java

--- FuelSales2.java
+++ FuelSales2.java
@@ -1,3 +1,4 @@
+
 /*modified on 
 Mar 04, 2023
  * developed by Bahati308
@@ -5,117 +6,127 @@ Mar 04, 2023
  * easy management of a fuel station
  */
 import java.util.*;
- class FuelSales {
-    public static void main(String[] args) {
-    try (Scanner scan = new Scanner (System.in)) {
-        // these are litres read from the tanks
-        System.out.println("----MORNING SECTION-----");
-        System.out.println();
 
-        System.out.println("****PMS****");
-        System.out.print("Morning dips pms: ");
-        double morning_dips_pms = scan.nextDouble();
+class FuelSales {
+        public static void main(String[] args) {
+                try (Scanner scan = new Scanner(System.in)) {
+                        // these are litres read from the tanks
+                        System.out.println("----MORNING SECTION-----");
+                        System.out.println();
 
-        System.out.print("day opening pms :");
-        double day_opening_pms = scan.nextDouble();
+                        System.out.println("****PMS****");
+                        System.out.print("Morning dips pms: ");
+                        double morning_dips_pms = scan.nextDouble();
 
-        System.out.print("day closing pms :");
-        double day_closing_pms = scan.nextDouble();
+                        System.out.print("day opening pms :");
+                        double day_opening_pms = scan.nextDouble();
 
-        System.out.print("PMS RATE : ");
-        double rate_pms = scan.nextDouble();
+                        System.out.print("day closing pms :");
+                        double day_closing_pms = scan.nextDouble();
 
-        double day_pms_litres =day_closing_pms-day_opening_pms;
-        System.out.println("Day Shift PMS Litres : "+day_pms_litres);
+                        System.out.print("PMS RATE : ");
+                        double rate_pms = scan.nextDouble();
 
+                        double day_pms_litres = day_closing_pms - day_opening_pms;
+                        System.out.println("Day Shift PMS Litres : " + day_pms_litres);
 
-        double day_sales_pms = day_pms_litres*rate_pms;
-        System.out.println("Day sales PMS : "+day_sales_pms);
+                        double day_sales_pms = day_pms_litres * rate_pms;
+                        System.out.println("Day sales PMS : " + day_sales_pms);
 
-        double pms_morning_diff =(day_pms_litres-morning_dips_pms);
+                        double pms_morning_diff = (day_pms_litres - morning_dips_pms);
 
-        if (pms_morning_diff==0) {
+                        if (pms_morning_diff == 0) {
+                                System.out.println("woow, we balance");
+                        }
+
+                        else if (pms_morning_diff > 0) {
+                                System.out.println("woow, we have excess of :" + pms_morning_diff + "litres");
+
+                        }
+
+                        else {
+                                System.out.println("soory, we have a shortage of :" + pms_morning_diff + "litres");
+                        }
+                        System.out.println();
+                        System.out.println("***AGO***");
+                        System.out.print("Morning dips ago: ");
+                        double morning_dips_ago = scan.nextDouble();
+                        System.out.println("ago morning dips : " + morning_dips_ago);
+
+                        System.out.print("day opening ago :");
+                        double day_opening_ago = scan.nextDouble();
+
+                        System.out.print("day closing ago :");
+                        double day_closing_ago = scan.nextDouble();
+
+                        System.out.print("AGO RATE : ");
+                        double rate_ago = scan.nextDouble();
+                        double day_ago_litres = day_closing_ago - day_opening_ago;
+                        System.out.println("Day Shift AGO Litres : " + day_ago_litres);
+
+                        double day_sales_ago = day_ago_litres * rate_ago;
+                        System.out.println("Day sales AGO : " + day_sales_ago);
+                        System.out.println();
+
+                        double ago_morning_diff =(day_ago_litres-morning_dips_ago);
+
+        if (ago_morning_diff==0) {
                 System.out.println("woow, we balance");
         }
 
-         else if (pms_morning_diff>0) {
-        System.out.println("woow, we have excess of :"+pms_morning_diff+"litres");
+         else if (ago_morning_diff>0) {
+        System.out.println("woow, we have excess of :"+ago_morning_diff+"litres");
                 
         }
         
                  else {
-        System.out.println("soory, we have a shortage of :"+pms_morning_diff+"litres");
+        System.out.println("soory, we have a shortage of :"+ago_morning_diff+"litres");
                  }
-        System.out.println();
-                 System.out.println("***AGO***");
-                 System.out.print("Morning dips ago: ");
-                 double morning_dips_ago = scan.nextDouble();
-         System.out.println("ago morning dips : "+ morning_dips_ago);
-         
-         
-                 System.out.print("day opening ago :");
-                 double day_opening_ago = scan.nextDouble();
-         
-                 System.out.print("day closing ago :");
-                 double day_closing_ago = scan.nextDouble();
-         
-                 System.out.print("AGO RATE : ");
-                 double rate_ago = scan.nextDouble();
-                 double day_ago_litres =day_closing_ago-day_opening_ago;
-                 System.out.println("Day Shift AGO Litres : "+day_ago_litres);
-         
-         
-                 double day_sales_ago = day_ago_litres*rate_ago;
-                 System.out.println("Day sales AGO : "+day_sales_ago);
-                 System.out.println();
+                
+                        System.out.println("---EVENING SECTION----");
+                        System.out.print("Evening dips PMS: ");
+                        double evening_dips_pms = scan.nextDouble();
+                        System.out.println("pms evening dips : " + evening_dips_pms);
 
-        System.out.println("---EVENING SECTION----");
-        System.out.print("Evening dips PMS: ");
-        double evening_dips_pms =scan.nextDouble();
-        System.out.println("pms evening dips : "+evening_dips_pms);
+                        System.out.println("night opening PMS :" + day_closing_pms);
 
-        System.out.println("night opening PMS :" + day_closing_pms);
+                        System.out.print("night closing pms :");
+                        double night_closing_pms = scan.nextDouble();
 
+                        double night_pms_litres = night_closing_pms - day_closing_pms;
+                        System.out.println("Night Shift PMS Litres : " + night_pms_litres);
 
-        System.out.print("night closing pms :");
-        double night_closing_pms = scan.nextDouble();
+                        double night_sales_pms = night_pms_litres * rate_pms;
+                        System.out.println("Night sales PMS : " + night_sales_pms);
 
-        double night_pms_litres =night_closing_pms-day_closing_pms;
-        System.out.println("Night Shift PMS Litres : "+night_pms_litres);
+                        double pms_litres = night_pms_litres + day_pms_litres;
+                        System.out.println("Total PMS Litres :" + pms_litres);
 
+                        double total_pms_sales = night_sales_pms + day_sales_pms;
+                        System.out.println("Total PMS Sales: " + total_pms_sales);
 
-        double night_sales_pms = night_pms_litres*rate_pms;
-        System.out.println("Night sales PMS : "+night_sales_pms);
+                        System.out.print("Evening dips AGO: ");
+                        double evening_dips_ago = scan.nextDouble();
+                        System.out.println("ago evening dips : " + evening_dips_ago);
 
-        double pms_litres = night_pms_litres+day_pms_litres;
-        System.out.println("Total PMS Litres :" +pms_litres);
+                        System.out.println("night opening AGO :" + day_closing_ago);
 
-        double total_pms_sales= night_sales_pms+day_sales_pms;
-        System.out.println("Total PMS Sales: "+ total_pms_sales);
-        
+                        System.out.print("night closing ago :");
+                        double night_closing_ago = scan.nextDouble();
 
-        System.out.print("Evening dips AGO: ");
-        double evening_dips_ago =scan.nextDouble();
-        System.out.println("ago evening dips : "+evening_dips_ago);
+                        double night_ago_litres = night_closing_ago - day_closing_ago;
+                        System.out.println("Night Shift AGO Litres : " + night_ago_litres);
 
-        System.out.println("night opening AGO :" + day_closing_ago);
+                        double night_sales_ago = night_ago_litres * rate_ago;
+                        System.out.println("Night sales AGO : " + night_sales_ago);
 
-        System.out.print("night closing ago :");
-        double night_closing_ago = scan.nextDouble();
+                        double ago_litres = night_ago_litres + day_ago_litres;
+                        System.out.println("Total AGO Litres :" + ago_litres);
 
-        double night_ago_litres =night_closing_ago-day_closing_ago;
-        System.out.println("Night Shift AGO Litres : "+night_ago_litres);
+                        double total_ago_sales = night_sales_ago + day_sales_ago;
+                        System.out.println("Total AGO Sales: " + total_ago_sales);
 
-        double night_sales_ago = night_ago_litres*rate_ago;
-        System.out.println("Night sales AGO : "+night_sales_ago);
-
-        double ago_litres = night_ago_litres+day_ago_litres;
-        System.out.println("Total AGO Litres :" +ago_litres);
-
-        double total_ago_sales= night_sales_ago+day_sales_ago;
-        System.out.println("Total AGO Sales: "+ total_ago_sales);
-
-//        wait for FuelSales v2.0.1
-    }
-   }
- }
+                        // wait for FuelSales v2.0.1
+                }
+        }
+}
